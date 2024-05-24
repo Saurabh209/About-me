4bb299cff67c891688ebddd024d8d9ce8378e402
Saurabh209 <saurabhhh2613@gmail.com>
2024-05-24 22:20:59 +0530

    nav bar update

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-const hamburger = document.querySelector('.hamburger');
-const navlinks = document.querySelector('.nav-links');
 
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('active');
-  navlinks.classList.toggle('show');
-});
 
 
 
